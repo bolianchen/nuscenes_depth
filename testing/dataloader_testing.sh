dc22a7a0842173283239684b9d292d90576a2069
bolianchen <bolianchen@gmail.com>
2022-05-19 23:41:33 +0800

    Update testing scripts

--- testing/dataloader_testing.sh
+++ testing/dataloader_testing.sh
@@ -20,19 +20,19 @@ python simulate_dataloader.py --data_path $NUSCENES_DIR \
 			      --how_to_gen_masks black \
 			      --log_dir testing_results/dataloader/color_black
 
-## generate masks by overlapping 2d bboxes ground-truth provided with the nuscenes dataset
+## do not generate masks
 python simulate_dataloader.py --data_path $NUSCENES_DIR \
 	                      --use_radar --use_lidar \
 			      --seg_mask none \
 		  	      --nuscenes_version v1.0-mini \
-			      --how_to_gen_masks maskrcnn \
-			      --log_dir testing_results/dataloader/none_maskrcnn
+			      --log_dir testing_results/dataloader/no_masks
 
 # Keyframe
+## generate masks by overlapping 2d bboxes ground-truth provided with the nuscenes dataset
 python simulate_dataloader.py --data_path $NUSCENES_DIR \
 	                      --use_radar --use_lidar \
 			      --seg_mask color \
 		  	      --nuscenes_version v1.0-mini \
 			      --use_keyframe \
 			      --how_to_gen_masks bbox \
-			      --log_dir testing_results/color_bbox
+			      --log_dir testing_results/dataloader/color_bbox
